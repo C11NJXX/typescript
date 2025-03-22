6e1a22ce46a6f07b8898aa6f1ae424c4b0af5dc0
C_11nJxx <c_11njxxsmailbox@sina.com>
2025-03-22 09:28:07 +0800

    Day3(AM) (04-04-07-Function Overloads)

---
+++ 04-More on Functions/04-07-Function Overloads/test.ts
@@ -0,0 +1,13 @@
+function makeDate(timestamp: number): Date;
+function makeDate(m: number, d: number, y: number): Date;
+function makeDate(mOrTimestamp: number, d?: number, y?: number): Date {
+    if (d !== undefined && y !== undefined) {
+        return new Date(y, mOrTimestamp, d);
+    } else {
+        return new Date(mOrTimestamp);
+    }
+}
+const d1 = makeDate(12345678);
+const d2 = makeDate(5, 5, 5);
+//没有需要 2 参数的重载，但存在需要 1 或 3 参数的重载。ts(2575)
+// const d3 = makeDate(1, 3);
